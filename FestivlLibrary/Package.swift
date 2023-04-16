702bd58025a5ea8ccc233841db184ff8b1d713c8
Woodrow Melling <woodymelling@gmail.com>
2023-04-16 10:47:26 -0600

    More merge commits

--- FestivlLibrary/Package.swift
+++ FestivlLibrary/Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "prerelease/1.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "navigation-beta"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.4"),
         .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.5.0"),
         .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.8.0"),
